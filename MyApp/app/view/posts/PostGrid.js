6cccd0d3a8590c480628fb2bd30e22f33128550f
marichu99 <martinmaati31@gmail.com>
2024-03-13 10:59:14 +0300

    changes to the grids

--- MyApp/app/view/posts/PostGrid.js
+++ MyApp/app/view/posts/PostGrid.js
@@ -1,59 +1,65 @@
-Ext.define("MyApp.view.posts.PostGrid", {
-  extend: "Ext.grid.Panel",
-  xtype: "postgrid",
-  reference: "postgrid",
-  controller: "postgridviewcontroller",
-  store: {
-    type: "posts",
-  },
-  tbar: [
-    {
-      text: "Add Post",
-      iconCls: "fas fa-plus",
-      listeners: {
-        click: "onAddPostClicked",
-      },
+Ext.define('MyApp.view.posts.PostGrid', {
+    extend: 'Ext.grid.Panel',
+    xtype: 'postgrid',
+    reference: 'postgrid',
+    controller: 'postgridviewcontroller',
+    store: {
+        type: 'posts'
+    },
+    tbar: [{
+        text: 'Add Post',
+        iconCls: 'fas fa-plus',
+        listeners: {
+            click: 'onAddPostClicked'
+        }
     },
     {
-      text: "Edit/View Post",
-      iconCls: "fas fa-pencil-alt",
-      listeners: {
-        click: "onEditClicked",
-      },
+        text: 'Edit/View Post',
+        iconCls: 'fas fa-pencil-alt',
+        listeners: {
+            click: 'onEditClicked'
+        }
     },
     {
-      text: "Delete Post",
-      iconCls: "far fa-trash-alt",
-      listeners: {
-        click: "onDeleteClicked",
-      },
+        text: 'Delete Post',
+        iconCls: 'far fa-trash-alt',
+        listeners: {
+            click: 'onDeleteClicked'
+        }
     },
     {
-      text: "Form fields",
-      listeners: {
-        click: "onFormFieldsClicked",
-      },
+        text: 'Form fields',
+        listeners: {
+            click: 'onFormFieldsClicked'
+        }
+    },
+     {
+        text: 'VTypes',
+        listeners: {
+            click: 'onVTypesClicked'
+        }
     },
     {
-      text: "Layouts",
-      listeners: {
-        click: "onLayoutsClicked",
-      },
+        text: 'Layouts',
+        listeners: {
+            click: 'onLayoutsClicked'
+        }
+    }],
+    columns: [
+        { dataIndex: '_id', text: 'ID' },
+        { dataIndex: 'title', text: 'Title', flex: 1 },
+        { dataIndex: 'body', text: 'Body', flex: 2, sortable: false },
+        { dataIndex: 'userId', text: 'User ID', sortable: false, hidden: false }
+    ],
+    selModel: {
+        selType: 'checkboxmodel',
+        mode: 'SINGLE'
     },
-  ],
-  columns: [
-    { dataIndex: "_id", text: "ID" },
-    { dataIndex: "title", text: "Title", flex: 1 },
-    { dataIndex: "body", text: "Body", flex: 2, sortable: false },
-    { dataIndex: "userId", text: "User ID", sortable: false, hidden: false },
-  ],
-  selModel: {
-    selType: "checkboxmodel",
-    mode: "SINGLE",
-  },
-  bbar: {
-    xtype: "pagingtoolbar",
-    displayInfo: true,
-  },
-  scrollable: true,
-});
+    bbar: {
+        xtype: 'pagingtoolbar',
+        displayInfo: true
+    },
+    scrollable: true,
+
+
+})
