7eeb7d5fb1f69e413a82af0d90d190fb0380ed06
marichu99 <martinmaati31@gmail.com>
2024-03-13 10:14:04 +0300

    changes to the mainmenu

--- MyApp/app/store/Menu.js
+++ MyApp/app/store/Menu.js
@@ -1,49 +1,58 @@
-Ext.define('MyApp.store.Menu',{
-  extend:'Ext.data.TreeStore',
-  alias:'store.menu',
+Ext.define("MyApp.store.Menu", {
+  extend: "Ext.data.TreeStore",
+  alias: "store.menu",
   root: {
-      text: 'Root',
-      expanded: true,
-      children: [
+    text: "Root",
+    expanded: true,
+    children: [
+      {
+        text: "Security",
+        iconCls: "fas fa-users-cog",
+        children: [
           {
-              text: 'Security',
-              iconCls:'fas fa-users-cog',
-              children:[
-                  {
-                      text:'Users',
-                      iconCls:'fa fa-users',
-                      className:'usergrid',
-                      leaf:true
-                  },
-                  {
-                      text: 'Todos',
-                      iconCls: 'fas fa-user-secret',
-                      className: 'todogrid',
-                      leaf: true
-                  }
-              ]
-            
+            text: "Users",
+            iconCls: "fa fa-users",
+            className: "usergrid",
+            leaf: true,
           },
           {
-              text: 'Static Data',
-              iconCls:'fa fa-database fa-lg',
-              children: [
-                  {
-                      text: 'Posts',
-                      iconCls: 'fa fa-users',
-                      className: 'postgrid',
-                      leaf: true
-                  },
-                  {
-                      text: 'Personnel',
-                      iconCls: 'fa fa-users',
-                      leaf:true,
-                      className: 'mainlist',
-                  }
-              ]
-
+            text: "Todos",
+            iconCls: "fas fa-user-secret",
+            className: "todogrid",
+            leaf: true,
           },
-          
-      ]
-  }
-})
+        ],
+      },
+      {
+        text: "Static Data",
+        iconCls: "fa fa-database fa-lg",
+        children: [
+          {
+            text: "Users",
+            iconCls: "fa fa-users",
+            className: "parentpanel",
+            leaf: true,
+          },
+          {
+            text: "Posts",
+            iconCls: "fa fa-users",
+            className: "postgrid",
+            leaf: true,
+          },
+          {
+            text: "Todos",
+            iconCls: "fa fa-users",
+            leaf: true,
+            className: "todogrid",
+          },
+          {
+            text: "Personnel",
+            iconCls: "fa fa-users",
+            leaf: true,
+            className: "mainlist",
+          },
+        ],
+      },
+    ],
+  },
+});
